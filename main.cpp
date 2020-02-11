27a2f94615abdad8757d96380243222201c19bfe
TonyAlbano420 <57347697+TonyAlbano420@users.noreply.github.com>
2020-02-11 18:52:44 +0100

    Add files via upload
    New:
    -Modifiche alla stampa, "macchina" viene direttamente stampata insieme al campo e la posizione viene aggiornata tramite coordinate
    -Aggiunti comandi di movimento
    
    Da fixare:
    -la stampa del cursore nel campo
    -la bufferizzazione della macchina

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <windows.h>
+#include "campo.hpp"
+#include <conio.h>
+#include <chrono>
+
+using namespace std;
+
+int x = 24;
+int y = 7;
+
+uint64_t time()
+{
+  using namespace std::chrono;
+  return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+}
+
+int getInput()
+{
+	if (kbhit())
+	{
+		return getch();
+	}
+	return 0;
+}
+
+void processInput(int c)
+{
+		// cout << c;
+		switch(c) {
+			case 72:
+				y--;
+				break;
+			case 80:
+				y++;
+				break;
+			case 77:
+				x++;
+				break;
+			case 75:
+				x--;
+				break;
+		}
+}
+
+int main(int argc, char const *argv[])
+{
+	Campo c;
+
+	int last = time();
+	int input = 0;
+
+	while (1)
+	{
+		int newInput = getInput();
+		if (newInput != 0)
+		{
+			input = newInput;
+		}
+
+		if (time() - last >= 50)
+		{
+			last = time();
+
+			processInput(input);
+			input = 0;
+
+			c.aggiorna();
+
+			system("cls");
+			c.stampa(y, x);
+		}
+	}
+
+	return 0;
+}
